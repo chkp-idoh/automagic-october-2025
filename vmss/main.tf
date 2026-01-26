fd0c981a57c3db85efedd816cb317af44d0f897e
chkp-idoh <92520013+chkp-idoh@users.noreply.github.com>
2026-01-26 14:27:13 +0200

    Update main.tf

--- vmss/main.tf
+++ vmss/main.tf
@@ -2,7 +2,7 @@
 locals {
     envId = var.envId
     rg =  "automagic-vmss-${local.envId}"
-    location = "westeurope"
+    location = "northeurope"
     vmss_name = "vmss-${local.envId}"
     vnet_name = "automagic-vmss-vnet-${local.envId}"
     vnet_address = "10.101.0.0/16"
