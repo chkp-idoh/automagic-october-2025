aaa0351107250cc8cded073b4ac25440c464c834
chkp-idoh <92520013+chkp-idoh@users.noreply.github.com>
2026-01-26 15:51:22 +0200

    Update net.tf

--- cluster/linux/net.tf
+++ cluster/linux/net.tf
@@ -3,7 +3,7 @@ resource "azurerm_public_ip" "public_ip" {
   location            = azurerm_resource_group.linux.location
   resource_group_name = azurerm_resource_group.linux.name
   allocation_method   = "Static"
-  sku                = "Standard"
+  sku                 = "Standard"
 
   lifecycle {
     ignore_changes = [
